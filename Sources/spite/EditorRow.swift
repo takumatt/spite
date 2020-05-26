fff3f65e1286ab85348af036a38f6d973d5716b1
Takuma Matsushita <takuma.matt@gmail.com>
2020-05-26 21:21:20 +0900

    update EditorRow

--- Sources/spite/EditorRow.swift
+++ Sources/spite/EditorRow.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct EditorRow {
+    
+    let size: Int
+    let chars: [char]
+    
+    init(size: Int = 0, chars: [char] = []) {
+        
+        self.size = size
+        self.chars = chars
+    }
+}
